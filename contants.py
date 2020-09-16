41602e5694861f6d089fcd55c19ee9feb2c752a1
Vasilis.Andrik <andvasf1@gmail.com>
2020-09-16 16:10:37 +0300

    extracted contastants to different file

---
+++ contants.py
@@ -0,0 +1,12 @@
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+dis_width = 600
+dis_height = 400
+
+snake_block = 10
+snake_speed = 15
--- snake.py
+++ snake.py
@@ -1,26 +1,13 @@
 import pygame
 import time
 import random
+from contants import white, yellow, black, red, green, blue, dis_width, dis_height, snake_block, snake_speed
 
 pygame.init()
-
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-
-dis_width = 600
-dis_height = 400
-
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game')
 
 clock = pygame.time.Clock()
-
-snake_block = 10
-snake_speed = 15
 
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
